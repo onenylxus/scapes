6bcf8eef36bdbefb5d376561c3086696c8997382
Nicholas Ng <onenylxus@gmail.com>
2022-07-01 12:36:41 +0800

    Tilemap class

---
+++ src/world/Tilemap.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////
+// Scapes v0.1.0
+// Voxel-based role-playing game
+// Nicholas Ng, 2022 MIT License
+////////////////////////////////////////////////////////////////
+
+// Include
+#include "Tilemap.h"
+
+// Constructor
+Tilemap::Tilemap(const char* path, const unsigned int &rows, const unsigned int &columns)
+{
+  this->texture = new Sprite(path);
+  this->rows = rows;
+  this->columns = columns;
+  this->tileWidth = 1.0f / columns;
+  this->tileHeight = 1.0f / rows;
+}
+
+// Destructor
+Tilemap::~Tilemap()
+{
+  delete this->texture;
+}
+
+// Bind texture
+void Tilemap::Bind(const unsigned int &index) const
+{
+  this->texture->Bind(index);
+}
+
+// Get sprite coordinates
+void Tilemap::GetSpriteCoordinates(const unsigned int &index, std::array<glm::vec2, 4> &coords) const
+{
+  float x = index % columns;
+  float y = index / rows;
+
+  coords[0] = glm::vec2((x + 1) * this->tileWidth, y * this->tileHeight);
+  coords[1] = glm::vec2(x * this->tileWidth, y * this->tileHeight);
+  coords[2] = glm::vec2(x * this->tileWidth, (y + 1) * this->tileHeight);
+  coords[3] = glm::vec2((x + 1) * this->tileWidth, (y + 1) * this->tileHeight);
+}
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
---
+++ src/world/Tilemap.h
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////
+// Scapes v0.1.0
+// Voxel-based role-playing game
+// Nicholas Ng, 2022 MIT License
+////////////////////////////////////////////////////////////////
+
+// Include
+#pragma once
+#include <array>
+#include <cmath>
+#include <glm/glm.hpp>
+#include "Sprite.h"
+
+// Tilemap class
+class Tilemap
+{
+  // Constructor and destructor
+  public:
+    Tilemap(const char* path, const unsigned int &rows, const unsigned int &columns);
+    virtual ~Tilemap();
+
+  // Properties
+  private:
+    Sprite* texture;
+    unsigned int rows;
+    unsigned int columns;
+    float tileWidth;
+    float tileHeight;
+
+  // Methods
+  public:
+    void Bind(const unsigned int &index) const;
+
+  // Getters
+  public:
+    void GetSpriteCoordinates(const unsigned int &index, std::array<glm::vec2, 4> &coords) const;
+};
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
