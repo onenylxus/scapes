327cfdf8cb6a71c26992175450f1c402b816716b
Nicholas Ng <onenylxus@gmail.com>
2023-03-04 01:49:47 +0800

    Update gitmodules in shell script

--- res/vendor/modify.sh
+++ res/vendor/modify.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Modifying files in vendor folder..."
+echo "[1/2] Update gitmodules..."
+git --git-dir=$(pwd)/.git submodule foreach --recursive git reset --hard
+git --git-dir=$(pwd)/.git submodule update --remote --merge
+echo "[2/2] Setup CMake in imgui folder..."
 cp -r $(pwd)/res/vendor/imgui/. $(pwd)/vendor/imgui/
 echo "Modify completed"
 echo ""
