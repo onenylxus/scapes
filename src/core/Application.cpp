fd4bcf60ea0a5426785cae469b9760df674b3d91
Nicholas Ng <onenylxus@gmail.com>
2023-04-24 23:45:52 +0800

    Condition checking for world console

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -48,18 +48,21 @@ void Application::OnRender()
   // Render world
   this->world->Render();
 
-  // Render OpenGL information through ImGui window
-  ImGui::SetNextWindowPos(ImVec2(0, 0));
-  ImGui::Begin("OpenGL Information", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoBackground);
-  ImGui::SetCursorPos(ImVec2(4.0f, 4.0f));
-  ImGui::Text("%s %s", this->title.c_str(), this->version.c_str());
-  ImGui::SetCursorPos(ImVec2(4.0f, 20.0f));
-  ImGui::Text("OpenGL company: %s", this->GetGLVendor().c_str());
-  ImGui::SetCursorPos(ImVec2(4.0f, 36.0f));
-  ImGui::Text("OpenGL renderer: %s", this->GetGLRenderer().c_str());
-  ImGui::SetCursorPos(ImVec2(4.0f, 52.0f));
-  ImGui::Text("OpenGL version: %s", this->GetGLVersion().c_str());
-  ImGui::End();
+  // Render ImGui window
+  if (this->world)
+  {
+    ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+    ImGui::Begin("OpenGL Information", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoBackground);
+    ImGui::SetCursorPos(ImVec2(4.0f, 4.0f));
+    ImGui::Text("%s %s", this->title.c_str(), this->version.c_str());
+    ImGui::SetCursorPos(ImVec2(4.0f, 20.0f));
+    ImGui::Text("OpenGL company: %s", this->GetGLVendor().c_str());
+    ImGui::SetCursorPos(ImVec2(4.0f, 36.0f));
+    ImGui::Text("OpenGL renderer: %s", this->GetGLRenderer().c_str());
+    ImGui::SetCursorPos(ImVec2(4.0f, 52.0f));
+    ImGui::Text("OpenGL version: %s", this->GetGLVersion().c_str());
+    ImGui::End();
+  }
 }
 
 // End event callback
